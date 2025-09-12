ddcfee11653e309fa9fd50e5c6c5fb6c7056e4c1
Octavian Marina <octamarina@gmail.com>
2025-09-12 14:12:44 +0300

    Avances

---
+++ README.md
@@ -0,0 +1,6 @@
+# AI From Scratch
+
+This repository contains implementations of AI algorithms from scratch.  
+The goal is to build and understand these algorithms step by step without using high-level machine learning frameworks.  
+
+Only basic libraries such as **NumPy** are used.
---
+++ ml-algos/logistic-regression/LogisticRegression.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticRegression:
+    def __init__(self, lr=0.0001, n_steps=1000):
+        self.lr = lr
+        self.n_steps = n_steps
+        self.bias = None
+        self.weights = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_steps):
+            linear_pred = X @ self.weights + self.bias
+            predictions = sigmoid(linear_pred)
+
+            dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
+            db = (1 / n_samples) * np.sum(predictions-y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        linear_pred = X @ self.weights + self.bias
+        predictions = sigmoid(linear_pred)
+        return predictions
